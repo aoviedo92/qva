6eb59cd83521bf078372330bb897fc6e79935f5e
Adrian Oviedo <adrian.oviedo.1992@gmail.com>
2017-10-30 11:48:12 -0400

    agregado la conf de db local a prod (sqlite), solo para probar q el hosting en pythonanywhere funciona

--- qvandares/settings/prod.py
+++ qvandares/settings/prod.py
@@ -13,6 +13,13 @@ CACHES = {
     }
 }
 
+# solo para probar q el hostin en pythonanywhere funciona
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
