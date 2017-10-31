aa71d985e55b527564cb107b32aa94f7179738d7
Adrian Oviedo <adrian.oviedo.1992@gmail.com>
2017-10-31 14:38:22 -0400

    STATICFILES_DIR debe estar en prod tambien para q collectstatic recoja sus estaticos

--- qvandares/settings/base.py
+++ qvandares/settings/base.py
@@ -95,3 +95,7 @@ MODELTRANSLATION_DEFAULT_LANGUAGE = 'en'  # explicito mejor que implicito
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, '../static'),
+)
--- qvandares/settings/dev.py
+++ qvandares/settings/dev.py
@@ -12,7 +12,3 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '../static'),
-)
