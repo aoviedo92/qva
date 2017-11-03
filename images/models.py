8a63aee00dcf8428365a463565cc4a1e6ff70d22
Adrian Oviedo <adrian.oviedo.1992@gmail.com>
2017-11-03 01:21:09 -0400

    mejoras y comentarios en el modelo photo

--- images/models.py
+++ images/models.py
@@ -16,25 +16,33 @@ class Photo(models.Model):
     original = models.ImageField(upload_to=photo_folder)
     thumbnail = models.ImageField(upload_to=photo_folder)
 
-    def make_thumbnail(self, image):
+    def make_thumbnail(self, image, photo_saved_name):
+        thumb_saved_name = 'thumb-' + photo_saved_name
         thumb = Img.open(image)
         thumb.thumbnail((200, 200), Img.ANTIALIAS)
         b_thumb = BytesIO()
         thumb.save(b_thumb, 'JPEG', quality=75)
-        self.thumbnail.save(name='%s-thumb.jpg' % uuid4(), content=b_thumb, save=False)
+        # llamar con save=false para no crear un ciclo de llamadas, ya q al hacer save llama al
+        # self.save q a su vez vuelve a llamar a make_thumbnail. aqui se llama a self.thumbnail.save
+        # y no se hace simplemente como self.original pq este fichero nunca se subio. ademas el self.original
+        # es conveniente setearlo por separado, primero la imagen 'resizada o no' y luego el nombre
+        self.thumbnail.save(name=thumb_saved_name, content=b_thumb, save=False)
 
     def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
         if self.original:
             image = Img.open(self.original)
             w, h = image.size
+            photo_saved_name = '%s.jpg' % uuid4()
             if w > 1200 and h > 800:
+                # resizar y hacer thumbnail con la imagen resizada obtenida
                 image = image.resize((Photo.DEFAULT_WIDTH, Photo.DEFAULT_HEIGHT), Img.ANTIALIAS)
                 image_resized = BytesIO()
                 image.save(image_resized, 'JPEG', quality=90)
                 self.original.file = image_resized
-                self.make_thumbnail(image_resized)
+                self.make_thumbnail(image_resized, photo_saved_name)
             else:
-                self.make_thumbnail(self.original)
-            self.original.name = '%s.jpg' % uuid4()
+                # si no hay q resizar solo hacer thumbnail con la img original
+                self.make_thumbnail(self.original, photo_saved_name)
+            self.original.name = photo_saved_name
 
         super().save(force_insert, force_update, using, update_fields)
