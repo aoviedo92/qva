3930df3fcf342389b7bac71ff07c4ef85c7d3675
Adrian Oviedo <adrian.oviedo.1992@gmail.com>
2017-11-09 20:13:14 -0500

    ad cambio moneda en base al usd

--- currency/templatetags/currency.py
+++ currency/templatetags/currency.py
@@ -1,9 +1,11 @@
 from django import template
-
+from moneyed import Money
+from djmoney_rates.utils import convert_money
 register = template.Library()
 
 
 @register.filter
 def set_currency(value, request):
     currency_session = request.session.get("currency", 'USD')
-    return '%d %s' % (value, currency_session)
+    # return '%d %s' % (value, currency_session)
+    return convert_money(value, "USD", currency_session)
