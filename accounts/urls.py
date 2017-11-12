0394eebb6c6a6b1da1af5c25f8bc5593318228d5
Adrian Oviedo <adrian.oviedo.1992@gmail.com>
2017-11-11 23:45:34 -0500

    ad reset-password support

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
+from django.contrib.auth import views as dj_auth_views
 from django.urls import reverse_lazy
 
 from . import views
-from django.contrib.auth import views as dj_auth_views
+
 urlpatterns = [
     # url(r'^login/$', views.user_login, name='login'),
     # url(r'^$', views.dashboard, name='dashboard'),
@@ -23,15 +24,33 @@ urlpatterns = [
         name='password_change'),
     # url(r'^password-change/$', 'django.contrib.auth.views.password_change', name='password_change'),
     # url(r'^password-change/done/$', 'django.contrib.auth.views.password_change_done', name='password_change_done'),
-    url(r'^password-change/done/$', dj_auth_views.PasswordChangeDoneView.as_view(template_name='accounts/password_change_done.html'), name='password_change_done'),
+    url(r'^password-change/done/$',
+        dj_auth_views.PasswordChangeDoneView.as_view(template_name='accounts/password_change_done.html'),
+        name='password_change_done'),
 
     # restore password urls
-    # url(r'^password-reset/$', 'django.contrib.auth.views.password_reset', name='password_reset'),
-    # url(r'^password-reset/done/$', 'django.contrib.auth.views.password_reset_done', name='password_reset_done'),
-    # url(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$',
-    #     'django.contrib.auth.views.password_reset_confirm', name='password_reset_confirm'),
-    # url(r'^password-reset/complete/$', 'django.contrib.auth.views.password_reset_complete',
-    #     name='password_reset_complete'),
+    url(r'^password-reset/$',
+        dj_auth_views.PasswordResetView.as_view(
+            template_name='accounts/password_reset_form.html',
+            email_template_name='accounts/password_reset_email.html',
+            subject_template_name='accounts/password_reset_subject.txt',
+            success_url=reverse_lazy('accounts:password_reset_done')
+        ),
+        name='password_reset'),
+    url(r'^password-reset/done/$',
+        dj_auth_views.PasswordResetDoneView.as_view(
+            template_name='accounts/password_reset_done.html'
+        ),
+        name='password_reset_done'),
+    url(r'^password-reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        dj_auth_views.PasswordResetConfirmView.as_view(
+            template_name='accounts/password_reset_confirm.html',
+            success_url=reverse_lazy('accounts:password_reset_complete')
+        ),
+        name='password_reset_confirm'),
+    url(r'^password-reset/complete/$',
+        dj_auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'),
+        name='password_reset_complete'),
 
     # user profiles
     # url(r'^users/$', views.user_list, name='user_list'),
