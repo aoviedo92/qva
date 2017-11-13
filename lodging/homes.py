345e9614a475df346bd2bda7bf498ff6ed59cec3
Adrian Oviedo <adrian.oviedo.1992@gmail.com>
2017-11-13 13:01:30 -0500

    *se cambio de FBV a CBV en home-list, esta CBV HomeList soporta pagindo y los multiples filtros pasados por get, los filtros q sean seteados se devuelven a la template para ponerles el valor seteado *el mixin seo.mixin.SeoMeta fue borrado por beneficio de django-meta app

--- lodging/homes.py
+++ lodging/homes.py
@@ -2,48 +2,47 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
-from meta.views import Meta
+from meta.views import Meta, MetadataMixin
 
 from index.mixins import MenuLinkMixin
 from lodging.models import Home
 from seo.mixins import ModelMetaView
 
+# FBV deprecated helper
+# def apply_filters(get_params):
+#     filters = {}
+#     cant_adults = get_params.get('cant_adults', '')
+#     min_price = get_params.get('min_price', '')
+#     max_price = get_params.get('max_price', '')
+#     # destination = get_request.get('destination', '')
+#     if cant_adults:
+#         filters.update({'max_guest': cant_adults})
+#     if min_price:
+#         filters.update({'price__gte': min_price})
+#     if max_price:
+#         filters.update({'price__lte': max_price})
+#     chosen_filters = {'cant_adults': cant_adults, 'min_price': min_price, 'max_price': max_price}
+#     qs = Home.objects.filter(**filters)
+#     return qs, chosen_filters
 
-def apply_filters(get_params):
-    filters = {}
-    cant_adults = get_params.get('cant_adults', '')
-    min_price = get_params.get('min_price', '')
-    max_price = get_params.get('max_price', '')
-    # destination = get_request.get('destination', '')
-    if cant_adults:
-        filters.update({'max_guest': cant_adults})
-    if min_price:
-        filters.update({'price__gte': min_price})
-    if max_price:
-        filters.update({'price__lte': max_price})
-    chosen_filters = {'cant_adults': cant_adults, 'min_price': min_price, 'max_price': max_price}
-    qs = Home.objects.filter(**filters)
-    return qs, chosen_filters
-
-
-def home_list(request):
-    homes, chosen_filters = apply_filters(request.GET)
-    paginator = Paginator(homes, 6)
-    page = request.GET.get('page', 1)
-    try:
-        homes = paginator.page(page)
-    except EmptyPage:
-        homes = paginator.page(paginator.num_pages)
-    except PageNotAnInteger:
-        homes = paginator.page(1)
-    context = {'homes': homes, MenuLinkMixin.use_context_var: 'homes'}
-    context.update(chosen_filters)
-    # TODO: add description, keywords
-    # TODO: i18n
-    context.update({'meta': Meta(use_title_tag=True, title='Homes & Rooms in Cuba',
-                                 description='description Homes & Rooms in Cuba',
-                                 keywords=['cuba', 'travel', 'rooms'])})
-    return render(request, 'homes/home-list.html', context)
+# FCV deprecated
+# def home_list(request):
+#     homes, chosen_filters = apply_filters(request.GET)
+#     paginator = Paginator(homes, 6)
+#     page = request.GET.get('page', 1)
+#     try:
+#         homes = paginator.page(page)
+#     except EmptyPage:
+#         homes = paginator.page(paginator.num_pages)
+#     except PageNotAnInteger:
+#         homes = paginator.page(1)
+#     context = {'homes': homes, MenuLinkMixin.use_context_var: 'homes'}
+#     context.update(chosen_filters)
+#
+#     context.update({'meta': Meta(use_title_tag=True, title='Homes & Rooms in Cuba',
+#                                  description='description Homes & Rooms in Cuba',
+#                                  keywords=['cuba', 'travel', 'rooms'])})
+#     return render(request, 'homes/home-list.html', context)
 
 
 class HomeDetail(ModelMetaView, DetailView):
@@ -59,16 +58,36 @@ class HomeDetail(ModelMetaView, DetailView):
         return context
 
 
-class HomeList(ListView):
+class HomeList(MetadataMixin, MenuLinkMixin, ListView):
+    # TODO: add GOOD description, keywords
+    # TODO: i18n
+    menu_link_active = 'homes'
+    title = 'Homes & Rooms in Cuba'
+    description = 'description Homes & Rooms in Cuba'
+    keywords = ['cuba', 'travel', 'rooms']
     model = Home
     template_name = 'homes/home-list.html'
     context_object_name = 'homes'
     paginate_by = 6
+    chosen_filters = {}
 
-    # def get_queryset(self):
-    #     qs = super().get_queryset()
-    #     get = self.request.GET
-    #     cant_adults = int(get.get('cant-adults', 0))
-    #     if cant_adults:
-    #         qs = qs.filter(max_guest=cant_adults)
-    #     return qs
+    def get_queryset(self):
+        # TODO: terminar los filtros destination y room_type
+        get_params = self.request.GET
+        filters = {}
+        cant_adults = get_params.get('cant_adults', '')
+        min_price = get_params.get('min_price', '')
+        max_price = get_params.get('max_price', '')
+        if cant_adults:
+            filters.update({'max_guest': cant_adults})
+        if min_price:
+            filters.update({'price__gte': min_price})
+        if max_price:
+            filters.update({'price__lte': max_price})
+        self.chosen_filters = {'cant_adults': cant_adults, 'min_price': min_price, 'max_price': max_price}
+        return Home.objects.filter(**filters)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(self.chosen_filters)
+        return context
--- lodging/urls.py
+++ lodging/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url, include
 from . import homes
 urlpatterns = [
     url(r'^homes/(?P<slug>[-\w]+)/$', homes.HomeDetail.as_view(), name='home_detail'),
-    url(r'^homes/', homes.home_list, name='home_list'),
-    # url(r'^homes/', homes.HomeList.as_view(), name='home_list'),
+    # url(r'^homes/', homes.home_list, name='home_list'),
+    url(r'^homes/', homes.HomeList.as_view(), name='home_list'),
 ]
